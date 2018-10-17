a6d619d426cbd16bb2f99e20b34a09427045dcb2
DaoNguyen108 <dao.nguyen@student.passerellesnumeriques.org>
2018-10-17 09:40:50 +0700

    fix error unused

--- app/src/main/java/com/pnv/matchmaking/love/listPictures/MainActivity.java
+++ app/src/main/java/com/pnv/matchmaking/love/listPictures/MainActivity.java
@@ -1,38 +1,111 @@
-package com.androidtutorialpoint.customgridview;
-
-import android.app.Activity;
+package com.pnv.matchmaking.love.listPictures;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.pnv.matchmaking.love.R;
 
-public class MainActivity extends Activity {
+import com.pnv.matchmaking.love.listPictures.Details;
 
-    GridView gridview;
+public class MainActivity extends AppCompatActivity {
 
-    public static String[] osNameList = {
-            "Android",
-            "iOS",
-            "Linux",
-            "MacOS",
-            "MS DOS",
-            "Symbian",
-            "Windows 10",
-            "Windows XP",
-    };
-    public static int[] osImages = {
-            R.mipmap.android,
-            R.mipmap.ios,
-            R.mipmap.linux,
-            R.mipmap.macos,
-            R.mipmap.msdos,
-            R.mipmap.symbian,
-            R.mipmap.windows10,
-            R.mipmap.winxp,};
+
+    GridView Grid;
+    com.pnv.matchmaking.love.listPictures.MainActivity.GRIDAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        gridview = (GridView) findViewById(R.id.customgrid);
-        gridview.setAdapter(new CustomAdapter(this, osNameList, osImages));
+        setContentView(R.layout.activitypicture_main);
+
+
+        final Integer[] image = new Integer[]{R.drawable.image1, R.drawable.image2, R.drawable.image3,
+                R.drawable.image4, R.drawable.image5, R.drawable.image6,
+                R.drawable.image7, R.drawable.image8};
+
+        Grid = (GridView)findViewById(R.id.MyGrid);
+        adapter = new com.pnv.matchmaking.love.listPictures.MainActivity.GRIDAdapter(getApplicationContext(), R.layout.gridpicture_activity, image);
+
+        Grid.setAdapter(adapter);
+        Grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent go = new Intent(getApplicationContext(),Details.class);
+                go.putExtra("image", image[position]);
+                //By position Clicked
+                startActivity(go);
+            }
+        });
+
+    }
+
+
+    public class GRIDAdapter extends ArrayAdapter {
+        private Integer[] Image;
+        private int resource;
+        private LayoutInflater inflater;
+
+        public GRIDAdapter(Context context, int resource, Integer[] image) {
+            super(context, resource, image);
+            Image = image;
+            this.resource = resource;
+            inflater = (LayoutInflater)com.pnv.matchmaking.love.listPictures.MainActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            com.pnv.matchmaking.love.listPictures.MainActivity.GRIDAdapter.ViewHolder Holder = null;
+
+            if (convertView == null) {
+                Holder = new com.pnv.matchmaking.love.listPictures.MainActivity.GRIDAdapter.ViewHolder();
+                convertView = inflater.inflate(resource, null);
+                Holder.IMAGE = (ImageView) convertView.findViewById(R.id.imageID);
+                convertView.setTag(Holder);
+            } else {
+                Holder = (com.pnv.matchmaking.love.listPictures.MainActivity.GRIDAdapter.ViewHolder)convertView.getTag();
+            }
+            Holder.IMAGE.setImageResource(Image[position]);
+            Holder.IMAGE.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            private ImageView IMAGE;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
