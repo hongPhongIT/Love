0df0716316b8a1f4d8abad8b7add4b1c5265ecd8
Đỗ Phú Khánh <dophukhanh2210@gmail.com>
2018-10-24 10:59:48 +0700

    Login Interface & Other Users Profile Page
    Login Interface and Other Users Profile Page (Interface that user see profile page of other people)

---
+++ app/src/main/java/com/pnv/matchmaking/love/profile/OtherUserProfile.java
@@ -0,0 +1,13 @@
+package com.pnv.matchmaking.love.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_user_profile);
+    }
+}
